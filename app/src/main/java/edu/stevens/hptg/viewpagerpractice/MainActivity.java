2ccb505ca84ca649507a7a08e57fd2f56675baf8
Pintaigao He <hptg1994@gmail.com>
2017-10-02 11:00:27 -0400

    Finish Debuging and The Teacher is useless

--- app/src/main/java/edu/stevens/hptg/viewpagerpractice/MainActivity.java
+++ app/src/main/java/edu/stevens/hptg/viewpagerpractice/MainActivity.java
@@ -22,10 +22,10 @@ public class MainActivity extends AppCompatActivity {
 		TabLayout tabLayout = (TabLayout) findViewById(R.id.view_pager_tab);
 		tabLayout.setupWithViewPager(viewPager);
 
-		tabLayout.getTabAt(0).setIcon(R.drawable.ic_favorite_white_24dp);
-		tabLayout.getTabAt(1).setIcon(R.drawable.ic_people_white_24dp);
-		tabLayout.getTabAt(2).setIcon(R.drawable.ic_phone_white_24dp);
-		tabLayout.getTabAt(3).setIcon(R.drawable.ic_settings_white_24dp);
+//		tabLayout.getTabAt(0).setIcon(R.drawable.ic_favorite_white_24dp);
+//		tabLayout.getTabAt(1).setIcon(R.drawable.ic_people_white_24dp);
+//		tabLayout.getTabAt(2).setIcon(R.drawable.ic_phone_white_24dp);
+//		tabLayout.getTabAt(3).setIcon(R.drawable.ic_settings_white_24dp);
 		// remove the shadow of action bar
 		getSupportActionBar().setElevation(0);
 	}
@@ -46,12 +46,10 @@ public class MainActivity extends AppCompatActivity {
 			return 4;
 		}
 
-		/*
 		//还有这里是把文字添加到TabView上面的
 		@Override
 		public CharSequence getPageTitle(int position) {
 			return "Page"  + position;
 		}
-		*/
 	}
 }
